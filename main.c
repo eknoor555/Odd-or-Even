2592480b42ee49e720af0ffb462c9e0d606ab223
eknoor555 <83641013+eknoor555@users.noreply.github.com>
2021-05-29 19:36:54 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a;
+    printf("Enter the no");
+    scanf("%d",&a);
+    if(a%2==0){
+        printf("%d is even",a);
+    }
+    else{
+        printf("%d is odd",a);
+    }
+    return 0;
+}
